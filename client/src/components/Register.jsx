f37b52ab13d53fe96be121e7dba59414850c0bd0
a1exandermalikov <alexander.malikov0619@gmail.com>
2025-05-30 01:20:59 +0500

    fixed

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -59,7 +59,7 @@ export function Register() {
 			options: {
 				emailRedirectTo: `${
 					window.location.origin
-				}/#/auth/callback?username=${encodeURIComponent(username)}`,
+				}/monkeytype/#/auth/callback?username=${encodeURIComponent(username)}`,
 			},
 		})
 
