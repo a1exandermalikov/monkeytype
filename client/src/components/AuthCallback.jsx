03da0fa09da1b16146cf6c6d8b6e4b5431620b8c
a1exandermalikov <alexander.malikov0619@gmail.com>
2025-05-30 01:27:38 +0500

    fixed

--- client/src/components/AuthCallback.jsx
+++ client/src/components/AuthCallback.jsx
@@ -7,13 +7,21 @@ export default function AuthCallback() {
 
 	useEffect(() => {
 		const handleAuth = async () => {
-			const hash = window.location.hash.substring(1)
-			const queryString = hash.includes('?') ? hash.split('?')[1] : ''
-			const params = new URLSearchParams(queryString)
+			const fullHash = window.location.hash.substring(1) // убираем первый #
+
+			// Разбиваем по второму #, например:
+			// fullHash = "/auth/callback?username=malikov#access_token=...&refresh_token=..."
+			const [pathWithQuery, tokenPart] = fullHash.split('#')
+
+			// Парсим username из pathWithQuery после ?
+			const query = new URLSearchParams(pathWithQuery.split('?')[1])
+			const username = query.get('username') || 'noname'
+
+			// Парсим токены из tokenPart
+			const params = new URLSearchParams(tokenPart || '')
 
 			const access_token = params.get('access_token')
 			const refresh_token = params.get('refresh_token')
-			const username = params.get('username') || 'noname'
 
 			if (access_token && refresh_token) {
 				const { error } = await supabase.auth.setSession({
@@ -26,7 +34,6 @@ export default function AuthCallback() {
 					return
 				}
 
-				// Получаем пользователя из сессии после установки
 				const {
 					data: { user },
 				} = await supabase.auth.getUser()
