5e51907ba47d4ff926954215626e29d53ea3784b
a1exandermalikov <alexander.malikov0619@gmail.com>
2025-05-31 00:21:51 +0500

    upd

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,9 +6,10 @@ import '../styles/Header.css'
 export function Header() {
 	const iconPath = './assets/icons/static'
 	const [avatarUrl, setAvatarUrl] = useState(null)
+	const [username, setUsername] = useState(null)
 
 	useEffect(() => {
-		const fetchAvatar = async () => {
+		const fetchUserData = async () => {
 			const {
 				data: { user },
 			} = await supabase.auth.getUser()
@@ -16,17 +17,18 @@ export function Header() {
 			if (user) {
 				const { data, error } = await supabase
 					.from('users')
-					.select('avatar_url')
+					.select('avatar_url, username')
 					.eq('id', user.id)
 					.single()
 
-				if (!error && data?.avatar_url) {
-					setAvatarUrl(data.avatar_url)
+				if (!error && data) {
+					if (data.avatar_url) setAvatarUrl(data.avatar_url)
+					if (data.username) setUsername(data.username)
 				}
 			}
 		}
 
-		fetchAvatar()
+		fetchUserData()
 	}, [])
 
 	return (
@@ -49,13 +51,15 @@ export function Header() {
 					<img className='icon' src={`${iconPath}/settings.png`} alt='#' />
 				</div>
 			</div>
+
 			<div className='account-area'>
 				<div className='notifications btn'>
 					<img className='icon' src={`${iconPath}/notifications.png`} alt='#' />
 				</div>
 
 				<Link to='/register'>
-					<div className='user-avatar btn'>
+					<div className='user-avatar'>
+						{username && <span className='username'>{username}</span>}
 						<img
 							className='icon avatar'
 							src={avatarUrl ? avatarUrl : `${iconPath}/user.png`}
